a30c3a85452107e78eb26b10c94c5f957210c515
Joshua Heidorn <heidornj@icloud.com>
2020-01-05 22:22:15 -0500

    cell rendering seems to be working, but updating the grid doesnt account for the draw status being part of "item"

--- components/ItemGrid.vue
+++ components/ItemGrid.vue
@@ -4,9 +4,20 @@
     :class="{ showborder: border, viewport: !split }"
     :ref="'container'"
   >
+    <!-- <span>
+      <p v-for="(value, index) in hitDetect">{{ index }} : {{ value }}</p>
+    </span> -->
     <canvas
       :id="0"
       :key="0"
+      :ref="'hit'"
+      :name="'hit'"
+      :height="canvasHeight"
+      :width="canvasWidth"
+    ></canvas>
+    <canvas
+      :id="1"
+      :key="1"
       :ref="'item'"
       :name="'item'"
       :height="canvasHeight"
@@ -14,8 +25,8 @@
       :class="{ showborder: border }"
     ></canvas>
     <canvas
-      :id="1"
-      :key="1"
+      :id="2"
+      :key="2"
       :ref="'overlay'"
       :name="'overlay'"
       :height="canvasHeight"
@@ -23,12 +34,14 @@
       :class="{ showborder: border }"
     ></canvas>
     <canvas
-      :id="2"
-      :key="2"
+      :id="3"
+      :key="3"
       :ref="'text'"
       :name="'text'"
       :height="canvasHeight"
       :width="canvasWidth"
+      v-on:click="hitCheck"
+      v-on:mousemove="showHitLocationValues"
     ></canvas>
   </div>
 </template>
@@ -76,10 +89,15 @@ export default {
   data() {
     return {
       cellCount: this.width * this.height,
-      layername: 'canvas' + this.counter,
-      counter: 0,
       cells: [], // base element for grid
-      items: [] // elements on grid
+      items: [], // elements on grid
+      hits: [],
+      hitDetect: {
+        clientX: 0,
+        clientY: 0,
+        offsetX: 0,
+        offsetY: 0
+      }
     }
   },
   computed: {
@@ -111,8 +129,9 @@ export default {
   created() {},
   mounted() {
     this.calculateCells()
-    this.addItem({ visible: true, count: 0, active: true }, 15) // visible is if image is shown, active is dim, count will be for bottle 'n shit
-    this.addItem({ visible: false, count: 0, active: false }, 20)
+    this.addItem({ visible: true, count: 0, active: true, drawn: false }, 15) // visible is if image is shown, active is dim, count will be for bottle 'n shit
+    this.addItem({ visible: true, count: 0, active: true, drawn: false }, 20)
+    this.addItem({ visible: true, count: 0, active: true, drawn: false }, 24)
     this.renderCells()
 
     // 2, 11, 16, 19, 31
@@ -124,6 +143,32 @@ export default {
   },
   provide() {},
   methods: {
+    showHitLocationValues(e) {
+      this.hitDetect.clientX = e.clientX
+      this.hitDetect.clientY = e.clientY
+      this.hitDetect.offsetX = e.offsetX
+      this.hitDetect.offsetY = e.offsetY
+    },
+    hitCheck(e) {
+      const hitCtx = this.$refs.hit.getContext('2d')
+      const pixel = hitCtx.getImageData(e.offsetX, e.offsetY, 1, 1).data
+      const color = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`
+
+      for (const hitbox in this.hits) {
+        if (this.hits[hitbox] === color) {
+          console.log(hitbox)
+          this.items[hitbox].active = !this.items[hitbox].active
+          this.updateCell(hitbox)
+        }
+      }
+      // console.log(color)
+    },
+    getRandomColor() {
+      const r = Math.round(Math.random() * 255)
+      const g = Math.round(Math.random() * 255)
+      const b = Math.round(Math.random() * 255)
+      return `rgb(${r},${g},${b})`
+    },
     /*
       calculateCells -- figures out how many cells we can fit, where they should start and stop based on canvas
       renderCells -- renders the cells including the item it contains
@@ -157,7 +202,10 @@ export default {
             x1: cols * this.imageSize,
             y1: rows * this.imageSize,
             x2: cols * this.imageSize * 2,
-            y2: rows * this.imageSize * 2
+            y2: rows * this.imageSize * 2,
+            overlay: {
+              darken: false
+            }
           })
           cellCounter = cellCounter + 1
         }
@@ -172,28 +220,47 @@ export default {
       }
     },
     renderCells() {
+      /* this should only be called once or when the whole table needs
+          to be re-rendered. something else needs to ne done for resize calcuarions
+      */
+
       // only render item (by id)
       let ctx = this.$refs.item.getContext('2d')
 
       for (const value in this.items) {
         const item = this.items[value]
         const cell = this.cells[value]
-        ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+
+        // dont render if the cell doesnt exist in our current context
+        if (typeof cell === 'undefined' || item.drawn) {
+          continue
+        }
+
+        item.drawn = true
+        // ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
         // image
 
         ctx = this.$refs.item.getContext('2d')
 
-        // if (item.active) {
-        ctx.beginPath()
-        ctx.lineWidth = '1'
-        ctx.strokeStyle = 'blue'
-        ctx.rect(cell.x1, cell.y1, this.imageSize, this.imageSize)
-        ctx.stroke()
-        // }
+        if (this.border) {
+          console.log('renderCells: border (%s)', value)
+          ctx.beginPath()
+          ctx.lineWidth = '1'
+          ctx.strokeStyle = 'blue'
+          ctx.rect(cell.x1, cell.y1, this.imageSize, this.imageSize)
+          ctx.stroke()
+        }
 
         if (item.visible) {
-          ctx = this.$refs.item.getContext('2d')
+          console.log('renderCells: text (%s)', value)
+          // sanity check
+          ctx = this.$refs.text.getContext('2d')
+          ctx.font = '15px Arial'
+          ctx.fillStyle = 'red'
+          ctx.fillText(`${value}`, cell.x1 + 30, cell.y1 + 40)
 
+          console.log('renderCells: image (%s)', value)
+          ctx = this.$refs.item.getContext('2d')
           ctx.beginPath()
           ctx.lineWidth = '1'
           ctx.strokeStyle = 'yellow'
@@ -205,14 +272,28 @@ export default {
             this.imageSize - this.padding
           )
           ctx.stroke()
-        }
-        // sanity check
-        ctx = this.$refs.text.getContext('2d')
-        ctx.font = '15px Arial'
-        ctx.fillStyle = 'red'
-        ctx.fillText(`${value}`, cell.x1 + 30, cell.y1 + 40)
 
-        if (!item.active) {
+          console.log('renderCells: hitbox (%s)', value)
+          // add hit color box
+          ctx = this.$refs.hit.getContext('2d')
+
+          ctx.beginPath()
+          ctx.lineWidth = '1'
+          const hitColor = this.getRandomColor()
+          this.hits[value] = hitColor
+          ctx.strokeStyle = hitColor
+          ctx.fillStyle = hitColor
+          ctx.fillRect(
+            cell.x1 + this.padding / 2,
+            cell.y1 + this.padding / 2,
+            this.imageSize - this.padding,
+            this.imageSize - this.padding
+          )
+          ctx.stroke()
+        }
+
+        console.log('renderCells: overlay (%s)', value)
+        if (!item.active && !cell.overlay.darken) {
           // add a fake dim overlay
           ctx = this.$refs.overlay.getContext('2d')
           ctx.beginPath()
@@ -220,25 +301,25 @@ export default {
           ctx.strokeStyle = 'black' // background color?
           ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'
           ctx.fillRect(cell.x1, cell.y1, this.imageSize, this.imageSize)
+          cell.overlay.darken = true
 
-          // adding something after drawing the above layer
-          ctx = this.$refs.item.getContext('2d')
-          ctx.beginPath()
-          ctx.lineWidth = '1'
-          ctx.strokeStyle = 'purple'
-          ctx.fillStyle = 'purple'
-          ctx.fillRect(cell.x1 + 20, cell.y1 + 20, 20, 20)
-          ctx.stroke()
+          // // adding something after drawing the above layer
+          // ctx = this.$refs.item.getContext('2d')
+          // ctx.beginPath()
+          // ctx.lineWidth = '1'
+          // ctx.strokeStyle = 'purple'
+          // ctx.fillStyle = 'purple'
+          // ctx.fillRect(cell.x1 + 20, cell.y1 + 20, 20, 20)
+          // ctx.stroke()
+
+          // remove the fake overlay
+          // if (!item.active) {
+          //   // add a fake dim overlay
+          //   ctx = this.$refs.overlay.getContext('2d')
+          //   ctx.clearRect(cell.x1, cell.y1, this.imageSize, this.imageSize)
+          // }
         }
-        // remove the fake overlay
-        // if (!item.active) {
-        //   // add a fake dim overlay
-        //   ctx = this.$refs.overlay.getContext('2d')
-        //   ctx.clearRect(cell.x1, cell.y1, this.imageSize, this.imageSize)
-        // }
       }
-
-      // this loop is for the item rendering itself
     },
 
     updateCells(cells) {
@@ -246,84 +327,76 @@ export default {
         this.updateCell(cell)
       }
     },
-    updateCell(cell) {
-      if (cell > 0) {
-        const ctx = this.$refs.item.getContext('2d')
+    updateCell(value) {
+      // maybe we should pass specific things to update into this? is it faster? does it matter?
+      if (value > 0) {
+        let ctx = this.$refs.item.getContext('2d')
 
-        const currentCell = this.cells[cell]
-        this.items[cell] = true
+        const item = this.items[value]
+        const cell = this.cells[value]
+
+        console.log('update', item, cell)
+
+        // dont render if the cell doesnt exist in our current context
+        if (typeof cell === 'undefined') {
+          return
+        }
 
         // this will add it to the "visible list" but not render it unless its relevent
-        if (cell <= this.cells.length) {
-          let rendercounter = cell
-          ctx.clearRect(
-            currentCell.x1,
-            currentCell.y1,
-            this.imageSize,
-            this.imageSize
-          )
-          console.log(currentCell.x1, currentCell.y1)
-          // cell
-          ctx.beginPath()
-          ctx.lineWidth = '1'
-          ctx.strokeStyle = 'blue'
-          ctx.rect(
-            currentCell.x1,
-            currentCell.y1,
-            this.imageSize,
-            this.imageSize
-          )
-          ctx.stroke()
+        if (value <= this.cells.length) {
+          if (this.border) {
+            console.log('updateCell: border (%s)', value)
+            ctx.beginPath()
+            ctx.lineWidth = '1'
+            ctx.strokeStyle = 'blue'
+            ctx.rect(cell.x1, cell.y1, this.imageSize, this.imageSize)
+            ctx.stroke()
+          }
 
-          // image
-          ctx.font = '15px Arial'
-          ctx.fillStyle = 'red'
-          ctx.fillText(
-            `${rendercounter + 1}`,
-            currentCell.x1 + 30,
-            currentCell.y1 + 40
-          )
-          rendercounter = rendercounter + 1
-          ctx.beginPath()
-          ctx.lineWidth = '1'
-          ctx.strokeStyle = 'yellow'
-          ctx.rect(
-            currentCell.x1 + this.padding / 2,
-            currentCell.y1 + this.padding / 2,
-            this.imageSize - this.padding,
-            this.imageSize - this.padding
-          )
-          ctx.stroke()
+          if (item.visible) {
+            console.log('updateCell: text (%s)', value)
+            // sanity check
+            ctx = this.$refs.text.getContext('2d')
+            ctx.font = '15px Arial'
+            ctx.fillStyle = 'red'
+            ctx.fillText(`${value}`, cell.x1 + 30, cell.y1 + 40)
+
+            console.log('updateCell: image (%s)', value)
+            ctx = this.$refs.item.getContext('2d')
+            ctx.beginPath()
+            ctx.lineWidth = '1'
+            ctx.strokeStyle = 'yellow'
+            ctx.fillStyle = 'yellow'
+            ctx.fillRect(
+              cell.x1 + this.padding / 2,
+              cell.y1 + this.padding / 2,
+              this.imageSize - this.padding,
+              this.imageSize - this.padding
+            )
+            ctx.stroke()
+          }
+
+          console.log('updateCell: overlay (%s)', value)
+          if (!item.active && !cell.overlay.darken) {
+            // add a fake dim overlay
+            ctx = this.$refs.overlay.getContext('2d')
+            ctx.beginPath()
+            ctx.lineWidth = '1'
+            ctx.strokeStyle = 'black' // background color?
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'
+            ctx.fillRect(cell.x1, cell.y1, this.imageSize, this.imageSize)
+            cell.overlay.darken = true
+          } else {
+            ctx = this.$refs.overlay.getContext('2d')
+            ctx.clearRect(cell.x1, cell.y1, this.imageSize, this.imageSize)
+            cell.overlay.darken = false
+          }
         }
       }
-      console.log(this.cells)
     },
     addItem(item, cell) {
       this.items[cell] = item
     },
-
-    renderItems() {
-      const ctx = this.$refs.item.getContext('2d')
-      const rendercounter = 1
-      for (const cell of this.cells) {
-        ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
-        // image
-        ctx.font = '15px Arial'
-        ctx.fillStyle = 'red'
-        ctx.fillText(`${rendercounter}`, cell.x1 + 30, cell.y1 + 40)
-
-        ctx.beginPath()
-        ctx.lineWidth = '1'
-        ctx.strokeStyle = 'yellow'
-        ctx.rect(
-          cell.x1 + this.padding / 2,
-          cell.y1 + this.padding / 2,
-          this.imageSize - this.padding,
-          this.imageSize - this.padding
-        )
-        ctx.stroke()
-      }
-    }, // draws image to cell
     ...mapMutations({
       context: 'canvas/setContext'
     })
